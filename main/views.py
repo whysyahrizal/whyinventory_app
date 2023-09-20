64d46dffff77b6ca2850ac8c5290b3f11da7e129
whysyahrizal <wahyu.sahrijal@ui.ac.id>
2023-09-20 11:53:35 +0700

    menyelesaikan tugas 3 keseluruhan

--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core import serializers
+from main.models import Item
+from main.forms import ItemForm
+from django.urls import reverse
 
 def show_main(request):
     context = {
@@ -7,3 +12,37 @@ def show_main(request):
     }
 
     return render(request, "main.html", context)
+
+def show_item(request) :
+    item = Item.objects.all()
+
+    context = {
+        "items" : item
+    }
+    return render(request, "show_item.html", context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_item'))
+
+    context = {'form': form}
+    return render(request, "create_item.html", context)
+
+def show_json(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_xml_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
