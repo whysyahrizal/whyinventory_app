035770e8bebe7a357fb5d948996dca06b843d9b4
whysyahrizal <wahyu.sahrijal@ui.ac.id>
2023-09-26 22:47:19 +0700

    Tugas 4

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, show_item, create_item, show_json, show_xml, show_xml_by_id, show_json_by_id
+from main.views import show_main, show_item, create_item, show_json, show_xml, show_xml_by_id, show_json_by_id, register, login_user, logout_user, add_item_amount, delete_item
 
 app_name = 'main'
 
@@ -11,4 +11,9 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'), 
+    path('logout/', logout_user, name='logout'),
+    path('add/<int:id>', add_item_amount, name='add_item_amount'),
+    path('delete/<int:id>', delete_item, name='delete_item'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,31 +1,77 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
 from main.models import Item
 from main.forms import ItemForm
 from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+import datetime
+
+
+def register(request) : 
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+@login_required(login_url='/login')
 def show_main(request):
     context = {
-        'name': 'Wahyu Syahrizal',
-        'class': 'PBP A'
+        'name': request.user.username,
+        'class': 'PBP A',
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
 
+@login_required(login_url='/login')
 def show_item(request) :
-    item = Item.objects.all()
+    item = Item.objects.filter(user=request.user)
 
     context = {
         "items" : item
     }
     return render(request, "show_item.html", context)
 
+@login_required(login_url='/login')
 def create_item(request):
     form = ItemForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        item = form.save(commit=False)
+        item.user = request.user
+        item.save()
         return HttpResponseRedirect(reverse('main:show_item'))
 
     context = {'form': form}
@@ -46,3 +92,16 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+@login_required(login_url='/login')
+def add_item_amount(request,id):
+    data = get_object_or_404(Item, pk=id)
+    data.amount+=1
+    data.save()
+    return HttpResponseRedirect(reverse('main:show_item'))
+
+@login_required(login_url='/login')
+def delete_item(request,id):
+    data = get_object_or_404(Item, pk=id)
+    data.delete()
+    return HttpResponseRedirect(reverse('main:show_item'))
